d1cde282c0f4b67462e56d486a07045c54793e98
Shlomi Fish <shlomif@shlomifish.org>
2022-04-20 18:48:58 +0300

    add output

--- mix.bash
+++ mix.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-rm -f first-to-11-cover.webm
+out="tiff-hurt.webm"
+rm -f "$out"
 # ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a -acodec libvorbis  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
 # ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
-ffmpeg -loop 1 -i IMG_2780.bmp -i Recording_35-amp.wav  -shortest -q:v 5 -acodec libvorbis -vcodec vp8 -s 640x480 first-to-11-cover.webm
+ffmpeg -loop 1 -i 15526594522_ba001951b9_c.bmp -i tiff1.wav  -shortest -q:v 5 -acodec libvorbis -vcodec vp8 -s 800x600 "$out"
