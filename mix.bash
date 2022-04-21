6dca08acd8d25216ee74389ca6db04baf8597ad1
Shlomi Fish <shlomif@shlomifish.org>
2022-04-21 11:41:07 +0300

    Rename variable/s to make them more descriptive.

--- mix.bash
+++ mix.bash
@@ -4,4 +4,4 @@ rm -f "$out"
 # ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a -acodec libvorbis  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
 # ffmpeg -loop 1 -i 14922538317_9528231ea1_c.bmp -i Recording_26.m4a  -shortest -q:v 5 -s 640x480 grimmie-cover.webm
 # ffmpeg -loop 1 -i 15526594522_ba001951b9_c.bmp -i tiff1.wav  -shortest -q:v 5 -acodec libvorbis -vcodec vp8 -s 800x600 "$out"
-ffmpeg -f image2 -i 15526594522_ba001951b9_c.bmp -i tiff1.wav -q:v 5 -acodec libvorbis -vcodec vp8 -s 800x600 "$out"
+ffmpeg -f image2 -i 15526594522_ba001951b9_c.bmp -i tiffany-cover.wav -q:v 5 -acodec libvorbis -vcodec vp8 -s 800x600 "$out"
